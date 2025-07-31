8e2c3e0bbe648fc1eadbc1cf89b38078038519df
Randy <raoulrandy3@gmail.com>
2025-07-31 09:35:50 +0300

    ADD and FETCH: Q4 post and some mistake

--- main.py
+++ main.py
@@ -6,17 +6,39 @@ from starlette.responses import JSONResponse
 
 app = FastAPI()
 
-@app.get("/ping")
-def getpong ()
-    return ("pong")
+class books:
+    author:str
+    title:str
+    content:str
+    creation_datetime:date;
 
-@app.get("/home")  
-    open("welcome.html","r",encoding="utf-8") as file
+@app.get("/ping")
+def getpong ():
+    return {f"pong"}
+
+@app.get("/home")
+def welcome ():
+    with open("welcome.html","r",encoding="utf-8") as file:
         html_content= file.read()
     return Response(content=html_content,status_code=200,media_type="text.html")
 
 @app.get("/{full_path:path}")
-    open("welcome.html","r",encoding="utf-8") as file
-        html_content= flie.read()
+def error():
+    with open("error.html","r",encoding="utf-8") as file:
+        html_content= file.read()
     return Response(content=html_content, status_code=404,media_type="text.html")
-        
+
+@app.post("/post")
+def post_book(books):
+    extend(books.author,books.title,books.content,books.creation_datetime)
+    if books.author is null:
+        return JSONResponse({"The author is missing"},status_code=400)
+    elif books.title is null:
+        return JSONResponse({f"The title is missing"},status_code=400)
+    elif books.content is null:
+        return JSONResponse({f"The content is missing"},status_code=400)
+    elif books.creation_datetime is null:
+        return JSONResponse({f"The creatime is missing"},status_code=400)
+    return JSONResponse({f"The book posting is successful"},status_code=200)
+
+
