aa7c8deae8061d84a2f87bff05bf0067f929c48f
Randy <raoulrandy3@gmail.com>
2025-07-31 08:41:54 +0300

    ADD: Q3 error 404

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from starlette.requests import Request
+from starlette.responses import Response
 from starlette.responses import JSONResponse
 
 app = FastAPI()
@@ -8,7 +9,14 @@ app = FastAPI()
 @app.get("/ping")
 def getpong ()
     return ("pong")
+
 @app.get("/home")  
     open("welcome.html","r",encoding="utf-8") as file
         html_content= file.read()
     return Response(content=html_content,status_code=200,media_type="text.html")
+
+@app.get("/{full_path:path}")
+    open("welcome.html","r",encoding="utf-8") as file
+        html_content= flie.read()
+    return Response(content=html_content, status_code=404,media_type="text.html")
+        
